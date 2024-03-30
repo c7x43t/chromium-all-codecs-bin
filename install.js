82a8b2844ae671a0e4eae5f5b653f655a6991b1f
Artur Morlang <a.morlang@tecoyo.com>
2024-03-30 17:06:10 +0100

    tar.xz cannot be unzipped revert to compatible chrome on linux

--- install.js
+++ install.js
@@ -3,10 +3,9 @@ const downloadURLs = {
   // this is a version of default chromium as fallback
   // https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2FLAST_CHANGE?alt=media
   //linux: 'https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F587811%2Fchrome-linux.zip?generation=1535668921668411&alt=media',
-  // linux: 'https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F1065920%2Fchrome-linux.zip?generation=1667312947566470&alt=media',
+  linux: 'https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F1065920%2Fchrome-linux.zip?generation=1667312947566470&alt=media',
   // linux: 'https://github.com/macchrome/linchrome/releases/download/v116.5845.193-M116.0.5845.193-r1160321-portable-ungoogled-Lin64/ungoogled-chromium_116.0.5845.193_1.vaapi_linux.tar.xz',
-  linux:
-    "https://github.com/macchrome/linchrome/releases/download/v122.6261.115-M122.0.6261.115-r1250580-portable-ungoogled-Lin64/ungoogled-chromium_122.0.6261.115_1.vaapi_linux.tar.xz",
+  // linux: "https://github.com/macchrome/linchrome/releases/download/v122.6261.115-M122.0.6261.115-r1250580-portable-ungoogled-Lin64/ungoogled-chromium_122.0.6261.115_1.vaapi_linux.tar.xz",
   //mac: 'https://github.com/macchrome/macstable/releases/download/v70.0.3538.67-r587811-macOS/Chromium.70.0.3538.67.nosync.app.zip',
   // mac: 'https://github.com/macchrome/macstable/releases/download/v106.5249.061-M106.0.5249.61-r1036826-macOS/Chromium.app.ungoogled-106.0.5249.61.zip',
   // mac: 'https://github.com/macchrome/macstable/releases/download/v117.5938.112-M117.0.5938.112-r1181205-macOS/Chromium.app.ungoogled-117.0.5938.112.tar.xz',
@@ -23,49 +22,63 @@ const downloadURLs = {
     "https://github.com/macchrome/winchrome/releases/download/v123.6312.56-M123.0.6312.56-r1262506-Win64/ungoogled-chromium-123.0.6312.56-1_Win64.7z",
 };
 
-const os = require( 'os' )
-const fs = require( 'fs' )
-const path = require( 'path' )
+const os = require('os');
+const fs = require('fs');
+const path = require('path');
 
-const rimraf = require( 'rimraf' )
-const extractZip = require( 'extract-zip' )
-const crossZip = require( 'cross-unzip' )
-const makeDir = require( 'make-dir' )
+const rimraf = require('rimraf');
+const extractZip = require('extract-zip');
+const crossZip = require('cross-unzip');
+const makeDir = require('make-dir');
 
-function unzip ( zipPath, dirPath, callback ) {
-  const ext = path.extname( zipPath )
-  if ( ext.indexOf( '7z' ) >= 0 ) {
-    return crossZip.unzip( zipPath, dirPath, callback )
+function unzip(zipPath, dirPath, callback) {
+  const ext = path.extname(zipPath);
+  if (ext.indexOf('7z') >= 0) {
+    return crossZip.unzip(zipPath, dirPath, callback);
+  } else if (ext.indexOf('tar.xz') >= 0) {
+    const exec = require('child_process').exec;
+    const cmd = `tar -xvf ${zipPath} -C ${dirPath}`;
+    exec(cmd, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+      }
+      if (stderr) {
+        console.error(`stderr: ${stderr}`);
+        return;
+      }
+      callback();
+    });
   }
-  return extractZip( zipPath, { dir: dirPath }, callback )
+  return extractZip(zipPath, { dir: dirPath }, callback);
 }
 
-const https = require( 'https' )
-const http = require( 'http' )
+const https = require('https');
+const http = require('http');
 
-const redstar = require( 'redstar' )
+const redstar = require('redstar');
 
-let revision = 706915
+let revision = 706915;
 
-main()
+main();
 
-async function main () {
-  const projectRoot = __dirname
-  const downloadRootDirectory = path.join( projectRoot, '.local-chromium-all-codecs' )
+async function main() {
+  const projectRoot = __dirname;
+  const downloadRootDirectory = path.join(projectRoot, '.local-chromium-all-codecs');
 
-  let platform = require( './get-platform.js' )
+  let platform = require('./get-platform.js');
 
-  if ( !platform ) throw new Error( 'Unspported platform: ' + p )
+  if (!platform) throw new Error('Unspported platform: ' + p);
 
-  if ( platform === 'mac' ) {
+  if (platform === 'mac') {
     // use older/stable version for mac
     // had many page crashes when playing youtube videos
     // could not find reliable issue after few days of debugging
-    revision = 587811
+    revision = 587811;
   }
 
-  if ( platform === 'linux' ) {
-    revision = 587811
+  if (platform === 'linux') {
+    revision = 587811;
 
     // console.log()
     // console.log( 'See download/install instructions from here: ' )
@@ -75,175 +88,178 @@ async function main () {
     // return
   }
 
-  let execName = 'chrome.exe'
-  let execPath = ''
-  let downloadDirectory = downloadRootDirectory
-  let filename = ''
-  if ( platform === 'mac' ) {
-    downloadDirectory = path.join( downloadRootDirectory, 'mac-' + revision )
-    execName = 'Chromium'
-  } else if ( platform === 'linux' ) {
-    downloadDirectory = path.join( downloadRootDirectory, 'linux-' + revision )
-    filename = downloadURLs[ 'linux' ]
-      .split( '/' ).pop()
-      .replace( /\?.*/, '' )
-    execName = 'chrome'
-  } else if ( platform === 'win32' ) {
-    downloadDirectory = path.join( downloadRootDirectory, 'win32-' + revision )
-    filename = downloadURLs[ 'win32' ]
-      .split( '/' ).pop()
-      .replace( /\?.*/, '' )
-    execName = 'chrome.exe'
-  } else if ( platform === 'win64' ) {
-    downloadDirectory = path.join( downloadRootDirectory, 'win64-' + revision )
-    filename = downloadURLs[ 'win64' ]
-      .split( '/' ).pop()
-      .replace( /\?.*/, '' )
-    execName = 'chrome.exe'
-  } else throw new Error( 'Unspported platform: ' + platform )
+  let execName = 'chrome.exe';
+  let execPath = '';
+  let downloadDirectory = downloadRootDirectory;
+  let filename = '';
+  if (platform === 'mac') {
+    downloadDirectory = path.join(downloadRootDirectory, 'mac-' + revision);
+    execName = 'Chromium';
+  } else if (platform === 'linux') {
+    downloadDirectory = path.join(downloadRootDirectory, 'linux-' + revision);
+    filename = downloadURLs['linux']
+      .split('/').pop()
+      .replace(/\?.*/, '');
+    execName = 'chrome';
+  } else if (platform === 'win32') {
+    downloadDirectory = path.join(downloadRootDirectory, 'win32-' + revision);
+    filename = downloadURLs['win32']
+      .split('/').pop()
+      .replace(/\?.*/, '');
+    execName = 'chrome.exe';
+  } else if (platform === 'win64') {
+    downloadDirectory = path.join(downloadRootDirectory, 'win64-' + revision);
+    filename = downloadURLs['win64']
+      .split('/').pop()
+      .replace(/\?.*/, '');
+    execName = 'chrome.exe';
+  } else throw new Error('Unspported platform: ' + platform);
 
   // find bin path through glob pattern
-  const execPromise = new Promise(function ( resolve ) {
-    redstar( path.join( downloadDirectory, '**/' + execName ), function ( err, files, dirs ) {
-      resolve( files[ 0 ] || '' )
-    } )
-  })
+  const execPromise = new Promise(function (resolve) {
+    redstar(path.join(downloadDirectory, '**/' + execName), function (err, files, dirs) {
+      resolve(files[0] || '');
+    });
+  });
 
-  execPath = await execPromise
+  execPath = await execPromise;
 
-  if ( execPath ) {
-    execPath = path.relative( process.cwd(), execPath )
+  if (execPath) {
+    execPath = path.relative(process.cwd(), execPath);
   }
 
   // make sure download directory exists
-  makeDir.sync( downloadDirectory )
+  makeDir.sync(downloadDirectory);
 
   try {
-    const s = fs.statSync( execPath )
-    const absExecPath = path.resolve( execPath )
-    fs.writeFileSync( path.join( __dirname, 'bin-path.txt' ), absExecPath, 'utf8' )
+    const s = fs.statSync(execPath);
+    const absExecPath = path.resolve(execPath);
+    fs.writeFileSync(path.join(__dirname, 'bin-path.txt'), absExecPath, 'utf8');
     // exists already, no need to download
-    console.log( 'Chromium already exists, no need to download.' )
-  } catch ( err ) {
+    console.log('Chromium already exists, no need to download.');
+  } catch (err) {
     // doesn't exist, need to download
-    console.log( 'need to download' )
-    const url = downloadURLs[ platform ]
+    console.log('need to download');
+    const url = downloadURLs[platform];
 
-    download( url )
-    function download ( url ) {
-      const params = require( 'url' ).parse( url )
+    download(url);
+    function download(url) {
+      const params = require('url').parse(url);
       // params.port = 443
       // params.method = 'GET'
 
       // console.log( params )
 
-      const h = ( params.protocol === 'https:' ) ? https : http
+      const h = (params.protocol === 'https:') ? https : http;
 
-      const destinationPath = path.join( downloadDirectory, `download-${ platform }-${ revision }-${ filename }` )
+      const destinationPath = path.join(downloadDirectory, `download-${platform}-${revision}-${filename}`);
+      if (fs.existsSync(destinationPath)) {
+        console.log(`${destinationPath} already exists. Skipping download.`);
+        return onFinish(); // Exit the function to skip the download
+      }
+      let downloadedBytes = 0;
+      let totalBytes = 0;
 
-      let downloadedBytes = 0
-      let totalBytes = 0
-
-      console.log( 'downloading...' )
-      const req = h.request( params, function ( res ) {
+      console.log('downloading...');
+      const req = h.request(params, function (res) {
         // handle redirects
-        if ( res.statusCode >= 300 && res.statusCode < 400 && res.headers.location ) {
-          console.log( '(redirect: ' + res.statusCode + ')' )
-          return download( res.headers.location )
+        if (res.statusCode >= 300 && res.statusCode < 400 && res.headers.location) {
+          console.log('(redirect: ' + res.statusCode + ')');
+          return download(res.headers.location);
         }
 
-        if ( res.statusCode !== 200 ) {
-          const error = new Error( `Download failed: server returned code ${ res.statusCode }. URL: ${ url }` )
+        if (res.statusCode !== 200) {
+          const error = new Error(`Download failed: server returned code ${res.statusCode}. URL: ${url}`);
           // consume response data to free up memory
-          res.resume()
-          throw error
+          res.resume();
+          throw error;
         }
-        console.log( 'res.status: ' + res.statusCode )
+        console.log('res.status: ' + res.statusCode);
 
-        totalBytes = Number( res.headers[ 'content-length' ] )
+        totalBytes = Number(res.headers['content-length']);
 
-        const file = fs.createWriteStream( destinationPath )
-        file.on( 'finish', onFinish )
-        file.on( 'error', onError )
-        res.pipe( file )
+        const file = fs.createWriteStream(destinationPath);
+        file.on('finish', onFinish);
+        file.on('error', onError);
+        res.pipe(file);
 
-        res.on( 'data', onData )
-      } )
+        res.on('data', onData);
+      });
 
-      req.on( 'error', onError )
-      req.end()
+      req.on('error', onError);
+      req.end();
 
-      function onData ( chunk ) {
-        downloadedBytes += chunk.length
-        onProgress( downloadedBytes, totalBytes )
+      function onData(chunk) {
+        downloadedBytes += chunk.length;
+        onProgress(downloadedBytes, totalBytes);
       }
 
-      function onFinish () {
-        console.log( 'Download Finished! Unzipping...' )
+      function onFinish() {
+        console.log('Download Finished! Unzipping...');
 
         // unzip
-        const zipPath = destinationPath
-        unzip(zipPath, downloadDirectory, async function ( err ) {
-          if ( err ) throw err
-          console.log( 'Unzip Success!' )
+        const zipPath = destinationPath;
+        unzip(zipPath, downloadDirectory, async function (err) {
+          if (err) throw err;
+          console.log('Unzip Success!');
 
           // find bin path through glob pattern
-          const execPromise = new Promise(function ( resolve ) {
-            redstar( path.join( downloadDirectory, '**/' + execName ), function ( err, files, dirs ) {
-              resolve( files[ 0 ] || '' )
-            } )
-          })
+          const execPromise = new Promise(function (resolve) {
+            redstar(path.join(downloadDirectory, '**/' + execName), function (err, files, dirs) {
+              resolve(files[0] || '');
+            });
+          });
 
-          execPath = await execPromise
+          execPath = await execPromise;
 
-          if ( execPath ) {
-            execPath = path.relative( process.cwd(), execPath )
+          if (execPath) {
+            execPath = path.relative(process.cwd(), execPath);
           }
 
           try {
-            const s = fs.statSync( execPath )
-            const absExecPath = path.resolve( execPath )
-            fs.writeFileSync( path.join( __dirname, 'bin-path.txt' ), absExecPath, 'utf8' )
+            const s = fs.statSync(execPath);
+            const absExecPath = path.resolve(execPath);
+            fs.writeFileSync(path.join(__dirname, 'bin-path.txt'), absExecPath, 'utf8');
 
             // exists already, no need to download
-            console.log( 'Chromium downloaded successfully!' )
-            console.log( 'Chromium: ' + execPath )
+            console.log('Chromium downloaded successfully!');
+            console.log('Chromium: ' + execPath);
 
             // delete zipfile
-            rimraf.sync( zipPath )
-          } catch ( err ) {
+            rimraf.sync(zipPath);
+          } catch (err) {
             // should exist now that we just downloaded it...
-            throw new Error( 'Failed to download Chromium' )
+            throw new Error('Failed to download Chromium');
           }
-        } )
+        });
       }
 
-      function onError ( err ) {
-        throw err
+      function onError(err) {
+        throw err;
       }
     }
   }
 }
 
-let progressBar = null
-let lastDownloadedBytes = 0
-function onProgress ( downloadedBytes, totalBytes ) {
-  if ( !progressBar ) {
+let progressBar = null;
+let lastDownloadedBytes = 0;
+function onProgress(downloadedBytes, totalBytes) {
+  if (!progressBar) {
     // initialize progress bar if it doesn't exist
-    const ProgressBar = require( 'progress' )
-    progressBar = new ProgressBar( `Downloading Chromium r${ revision } - ${ toMegabytes( totalBytes ) } [:bar] :percent :etas `, {
+    const ProgressBar = require('progress');
+    progressBar = new ProgressBar(`Downloading Chromium r${revision} - ${toMegabytes(totalBytes)} [:bar] :percent :etas `, {
       complete: '=',
       incomplete: ' ',
       width: 20,
       total: totalBytes
-    } )
+    });
   }
-  const delta = downloadedBytes - lastDownloadedBytes
-  lastDownloadedBytes = downloadedBytes
-  progressBar.tick( delta )
+  const delta = downloadedBytes - lastDownloadedBytes;
+  lastDownloadedBytes = downloadedBytes;
+  progressBar.tick(delta);
 }
 
-function toMegabytes ( bytes ) {
-  const mb = bytes / 1024 / 1024
-  return `${Math.round( mb * 10 ) / 10} Mb`
+function toMegabytes(bytes) {
+  const mb = bytes / 1024 / 1024;
+  return `${Math.round(mb * 10) / 10} Mb`;
 }
