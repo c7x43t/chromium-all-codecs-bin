5a7eb18d79308198fb7ddcd7e7bcf07f2f3cb2bd
Artur Morlang <a.morlang@tecoyo.com>
2024-03-31 13:18:33 +0200

    fix os import

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // give bin path to downloaded Chromium
 const fs = require('fs');
 const path = require('path');
+const os = require('os');
 const commandExists = require('command-exists');
 function getPlatform() {
   let platform = '';
